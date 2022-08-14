8824f2821455318a2b9e8bf34ac729e259e41d0d
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-14 17:21:47 +0530

    Linked list created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            Console.WriteLine("Linked list created.");
+        }
+    }
+}
